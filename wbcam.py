6bb1e80961b3d808740acc5526fa8487059173f7
Shreyas Kapale <shreyas.kapale@gmail.com>
2020-10-01 13:19:32 +0530

    Update wbcam.py

--- wbcam.py
+++ wbcam.py
@@ -1,9 +1,12 @@
 import cv2
 photo=cv2.imread("img.jpg")
-new_img=photo[100:240]
+x,y = input("Enter the cropping dimensions x and y : ").split()
+new_img=photo[y:x]
 while True:
     cv2.imshow("img",new_img)
-    if cv2.waitKey(0) & 0xFF==ord('q'):
+    if cv2.waitKey(0) & 0xFF==ord('s'):
+        filename = input("Enter the filename to save the cropped image")
+        cv2.imwrite(filename, new_img) 
         break
 cv2.destroyAllWindows()      
 
